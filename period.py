63e42a7cb8b2b0300df408a2e618bb9262898499
Joey Chen <jay.hatelove@gmail.com>
2020-09-21 17:01:28 +0800

    [refactor]extract method: without_overlapping() and merge if condition

--- period.py
+++ period.py
@@ -4,11 +4,13 @@ class Period(object):
         self.end = end
 
     def overlapping_days(self, another_period):
-        if self.start > self.end:
+        if self.start > self.end or self.without_overlapping(another_period):
             return 0
-        if self.end < another_period.start or self.start > another_period.end:
-            return 0
+
         overlapping_start = self.start if self.start > another_period.start else another_period.start
         overlapping_end = self.end if self.end < another_period.end else another_period.end
         delta = overlapping_end - overlapping_start
         return delta.days + 1
+
+    def without_overlapping(self, another_period):
+        return self.end < another_period.start or self.start > another_period.end
