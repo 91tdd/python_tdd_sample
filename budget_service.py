147ae37dfd3266f25d7bb47f44feccccb0c604bb
Joey Chen <jay.hatelove@gmail.com>
2020-09-21 17:05:59 +0800

    [feat]implement sum of overlapping_amount of multiple budgets

--- budget_service.py
+++ budget_service.py
@@ -4,9 +4,15 @@ from period import Period
 
 class BudgetService(object):
     def total_amount(self, start, end):
+        period = Period(start, end)
         budgets = get_budgets()
-        if len(budgets) > 0:
-            period = Period(start, end)
-            budget = budgets[0]
-            return budget.overlapping_amount(period)
-        return 0
+        total_amount = 0
+        for budget in budgets:
+            total_amount += budget.overlapping_amount(period)
+
+        return total_amount
+        # if len(budgets) > 0:
+        #     period = Period(start, end)
+        #     budget = budgets[0]
+        #     return budget.overlapping_amount(period)
+        # return 0
