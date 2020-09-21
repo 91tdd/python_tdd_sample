6c294bf8c044b293ff84be196259c45057794bfa
Joey Chen <jay.hatelove@gmail.com>
2020-09-21 16:30:01 +0800

    [refactor]extract method: overlapping_days()

--- budget_service.py
+++ budget_service.py
@@ -8,12 +8,13 @@ class BudgetService(object):
         if len(budgets) > 0:
             period = Period(start, end)
             budget = budgets[0]
-            if period.end < budget.first_day():
-                return 0
-            if period.start > budget.last_day():
-                return 0
-            return self.interval_days(period)
+            return self.overlapping_days(budget, period)
         return 0
+
+    def overlapping_days(self, budget, period):
+        if period.end < budget.first_day() or period.start > budget.last_day():
+            return 0
+        return self.interval_days(period)
 
     @staticmethod
     def interval_days(period):
