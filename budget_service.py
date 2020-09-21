8897b321b8115dad3c92f81c240adc692bf6696f
Joey Chen <jay.hatelove@gmail.com>
2020-09-21 15:32:38 +0800

    [refactor]extract page object: Period from start and end
    [refactor]extract page object: Period from start and end

--- budget_service.py
+++ budget_service.py
@@ -1,10 +1,16 @@
 from budget_repo import get_budgets
+from period import Period
 
 
 class BudgetService(object):
     def total_amount(self, start, end):
         budgets = get_budgets()
         if len(budgets) > 0:
-            delta = start - end
-            return delta.days + 1
+            period = Period(start, end)
+            return self.interval_days(period)
         return 0
+
+    @staticmethod
+    def interval_days(period):
+        delta = period.start - period.end
+        return delta.days + 1
---
+++ period.py
@@ -0,0 +1,4 @@
+class Period(object):
+    def __init__(self, start, end) -> None:
+        self.start = start
+        self.end = end
