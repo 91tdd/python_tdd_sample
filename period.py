c275f8b5e30146852184bbf6c06e227afa4fab36
Joey Chen <jay.hatelove@gmail.com>
2020-09-21 17:02:16 +0800

    [refactor]extract method is_valid()

--- period.py
+++ period.py
@@ -4,13 +4,16 @@ class Period(object):
         self.end = end
 
     def overlapping_days(self, another_period):
-        if self.start > self.end or self.without_overlapping(another_period):
+        if self.is_valid() or self.without_overlapping(another_period):
             return 0
 
         overlapping_start = self.start if self.start > another_period.start else another_period.start
         overlapping_end = self.end if self.end < another_period.end else another_period.end
         delta = overlapping_end - overlapping_start
         return delta.days + 1
+
+    def is_valid(self):
+        return self.start > self.end
 
     def without_overlapping(self, another_period):
         return self.end < another_period.start or self.start > another_period.end
