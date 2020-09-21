4aca9af0cd64608bf42f9e67cc77f08b38e16718
Joey Chen <jay.hatelove@gmail.com>
2020-09-21 15:11:37 +0800

    fake implementation: return 1 when there were any budget exist, else return 0

--- budget_service.py
+++ budget_service.py
@@ -4,4 +4,6 @@ from budget_repo import get_budgets
 class BudgetService(object):
     def total_amount(self, start, end):
         budgets = get_budgets()
+        if len(budgets) > 0:
+            return 1
         return 0
