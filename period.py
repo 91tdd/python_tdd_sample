e7eb63f782e5a772abe44088a1b0eefc0eae7227
Joey Chen <jay.hatelove@gmail.com>
2020-09-21 16:41:33 +0800

    [refactor]step 1, extract variables: "first_day" and "last_day", and create a variable "another_period" from budget.first_day() and budget.last_day()

--- period.py
+++ period.py
@@ -4,9 +4,12 @@ class Period(object):
         self.end = end
 
     def overlapping_days(self, budget):
-        if self.end < budget.first_day() or self.start > budget.last_day():
+        another_period = Period(budget.first_day(), budget.last_day())
+        first_day = budget.first_day()
+        last_day = budget.last_day()
+        if self.end < first_day or self.start > last_day:
             return 0
-        overlapping_start = self.start if self.start > budget.first_day() else budget.first_day()
-        overlapping_end = self.end if self.end < budget.last_day() else budget.last_day()
+        overlapping_start = self.start if self.start > first_day else first_day
+        overlapping_end = self.end if self.end < last_day else last_day
         delta = overlapping_start - overlapping_end
         return delta.days + 1
