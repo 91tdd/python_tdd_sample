51bf50caad4b12657b7031218f3e21b7ce0e3e20
Joey Chen <jay.hatelove@gmail.com>
2020-09-21 14:48:17 +0800

    [test]add failed test: no budgets

--- tests/test_budget_service.py
+++ tests/test_budget_service.py
@@ -1,9 +1,19 @@
 import unittest
+from datetime import date
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class BudgetService(object):
+    def total_amount(self, start, end):
+        pass
+
+
+class BudgetServiceTests(unittest.TestCase):
+    def test_no_budgets(self):
+        budget_service = BudgetService()
+        start = date(2020, 4, 1)
+        end = date(2020, 4, 1)
+        total_amount = budget_service.total_amount(start, end)
+        self.assertEqual(0, total_amount)
 
 
 if __name__ == '__main__':
