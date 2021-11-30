d9e3f7a58011452950b9087cfc536e6b94ed8394
Joey Chen <jay.hatelove@gmail.com>
2021-11-30 14:21:57 +0800

    [refactor] merge sequence if

--- src/budget_service.py
+++ src/budget_service.py
@@ -12,9 +12,7 @@ class BudgetService:
     def total_amount(self, start: date, end: date):
         budgets = get_budgets()
         if len(budgets) > 0:
-            if start > budgets[0].last_day():
-                return 0
-            if end < budgets[0].first_day():
+            if start > budgets[0].last_day() or end < budgets[0].first_day():
                 return 0
             delta = end - start
             return delta.days + 1
