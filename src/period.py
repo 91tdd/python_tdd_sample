3dcd5de7aa141a13787a3d1fa57a1eab45d5233b
Joey Chen <jay.hatelove@gmail.com>
2021-11-30 14:38:36 +0800

    [refactor] introduce temp variables: last_day and first_day

--- src/period.py
+++ src/period.py
@@ -5,8 +5,10 @@ class Period:
         self.start = start
 
     def get_overlapping_days(self, budget):
-        if self.start > budget.last_day() or self.end < budget.first_day():
+        last_day = budget.last_day()
+        first_day = budget.first_day()
+        if self.start > last_day or self.end < first_day:
             return 0
-        overlapping_start = self.start if self.start > budget.first_day() else budget.first_day()
-        overlapping_end = self.end if self.end < budget.last_day() else budget.last_day()
+        overlapping_start = self.start if self.start > first_day else first_day
+        overlapping_end = self.end if self.end < last_day else last_day
         return (overlapping_end - overlapping_start).days + 1
