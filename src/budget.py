b5176f5f7921681b1a319f2e1d61236a65310f1c
Joey Chen <jay.hatelove@gmail.com>
2021-11-30 14:18:29 +0800

    [fake] use 2020/4/1 as budget's first day

--- src/budget.py
+++ src/budget.py
@@ -1,5 +1,11 @@
+from datetime import date
+
+
 class Budget:
     def __init__(self, year_month, amount) -> None:
         super().__init__()
         self.amount = amount
         self.year_month = year_month
+
+    def first_day(self):
+        return date(2020, 4, 1)
