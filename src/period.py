690eb16c183f8e2e443e03720a39ea167d52b677
Joey Chen <jay.hatelove@gmail.com>
2021-11-30 14:45:00 +0800

    [feat] add invalid check of get_overlapping_days()

--- src/period.py
+++ src/period.py
@@ -5,6 +5,8 @@ class Period:
         self.start = start
 
     def get_overlapping_days(self, another):
+        if self.start > self.end:
+            return 0
         if self.start > another.end or self.end < another.start:
             return 0
         overlapping_start = self.start if self.start > another.start else another.start
