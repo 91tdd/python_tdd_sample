74ab816a3c0a0cf74a892c3d0d4f248909e690f6
Joey Chen <jay.hatelove@gmail.com>
2021-11-30 14:50:33 +0800

    [refactor] inline variable: daily_amount

--- src/budget_service.py
+++ src/budget_service.py
@@ -14,7 +14,6 @@ class BudgetService:
         budgets = get_budgets()
         if len(budgets) > 0:
             budget = budgets[0]
-            daily_amount = budget.daily_amount()
             period = Period(start, end)
-            return period.get_overlapping_days(budget.create_period()) * daily_amount
+            return period.get_overlapping_days(budget.create_period()) * budget.daily_amount()
         return 0
