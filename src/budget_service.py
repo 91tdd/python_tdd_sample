49380553673fceb0ed9e7d5c3338586f89bb4474
Joey Chen <jay.hatelove@gmail.com>
2021-11-30 14:10:54 +0800

    [test] add failed test: test_query_single_budget_month

--- src/budget_service.py
+++ src/budget_service.py
@@ -1,6 +1,11 @@
 from datetime import date
 
 
+def get_budgets():
+    pass
+
+
 class BudgetService:
     def total_amount(self, start: date, end: date):
+        budgets = get_budgets()
         return 0
--- tests/test_budget_service.py
+++ tests/test_budget_service.py
@@ -1,18 +1,35 @@
 import unittest
 from datetime import date
+from unittest.mock import patch
 
 from src.budget_service import BudgetService
+
+
+class Budget:
+    def __init__(self, year_month, amount) -> None:
+        super().__init__()
+        self.amount = amount
+        self.year_month = year_month
 
 
 class BudgetServiceTests(unittest.TestCase):
     def setUp(self):
         self.budget_service = BudgetService()
+        get_budgets_patcher = patch('src.budget_service.get_budgets')
+        self.fake_get_budgets = get_budgets_patcher.start()
 
     def test_no_budgets(self):
         self.total_amount_should_be(
             date(2020, 4, 1),
             date(2020, 4, 1),
             0)
+
+    def test_query_single_budget_month(self):
+        self.fake_get_budgets.return_value = [Budget('202004', 30)]
+        self.total_amount_should_be(
+            date(2020, 4, 1),
+            date(2020, 4, 30),
+            30)
 
     def total_amount_should_be(self, start, end, expected):
         self.assertEqual(expected, self.budget_service.total_amount(
