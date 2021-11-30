c4be1dfec83a5ade90a3ce2a60bfb05f96b10908
Joey Chen <jay.hatelove@gmail.com>
2021-11-30 14:29:47 +0800

    [refactor] introduce variable: overlapping_days

--- src/budget_service.py
+++ src/budget_service.py
@@ -13,9 +13,12 @@ class BudgetService:
         budgets = get_budgets()
         if len(budgets) > 0:
             budget = budgets[0]
+            overlapping_days = 0
             if start > budget.last_day() or end < budget.first_day():
-                return 0
-            overlapping_start = start if start > budget.first_day() else budget.first_day()
-            overlapping_end = end if end < budget.last_day() else budget.last_day()
-            return (overlapping_end - overlapping_start).days + 1
+                pass
+            else:
+                overlapping_start = start if start > budget.first_day() else budget.first_day()
+                overlapping_end = end if end < budget.last_day() else budget.last_day()
+                overlapping_days = (overlapping_end - overlapping_start).days + 1
+            return overlapping_days
         return 0
